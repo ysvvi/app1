f01f78b75526bbf9a056e8e12b66580acd9d75cc
Анастасия Романова <nastasia.romanova.nastya2000@mail.ru>
2025-01-30 14:01:04 +0300

    first

---
+++ main/tests.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.test import TestCase
+
+
+
---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    context = {
+        'title': 'Home',
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
