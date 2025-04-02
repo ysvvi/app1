32381cd3c745a9717dc4ad6de3e19e4281990a1b
ysvvi <nastasia.romanova.nastya2000@mail.ru>
2025-04-02 18:23:10 +0300

    Postgresql data base connected and full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,15 +1,22 @@
+
 from goods.models import Products
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 def q_search(query):
     if query.isdigit() and len(query) <= 7:
         return Products.objects.filter(id=int(query))
-    
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery("query")
+    return Products.objects.annotale(rank=SearchRank(vector, query)).order_by("-rank")
 
-    q_objects= Q()
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
+    #keywords = [word for word in query.split() if len(word) > 2]
 
-    return Products.objects.filter(q_objects)
+    #q_objects= Q()
+
+    #for token in keywords:
+        #q_objects |= Q(description__icontains=token)
+        #q_objects |= Q(name_icontains=token)
+
+    #return Products.objects.filter(q_objects)
