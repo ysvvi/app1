7eadad8cc167c02933b762c08d86607666040bca
ysvvi <nastasia.romanova.nastya2000@mail.ru>
2025-04-02 17:43:41 +0300

    simple search created

--- goods/urls.py
+++ goods/urls.py
@@ -4,7 +4,8 @@ from goods import views
 
 app_name = 'goods'
 
-urlpatterns = [
+urlpatterns = [ 
+    path("search/", views.catalog, name="search"),
     path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
