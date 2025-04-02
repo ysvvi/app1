1a45cabbc6f26c90d172641a02d48bfca66cc308
ysvvi <nastasia.romanova.nastya2000@mail.ru>
2025-04-02 19:33:46 +0300

    full text search well-done)

--- goods/utils.py
+++ goods/utils.py
@@ -1,14 +1,24 @@
-
 from goods.models import Products
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 
 def q_search(query):
+    if not query:
+        return Products.objects.none()  
+    
     if query.isdigit() and len(query) <= 7:
         return Products.objects.filter(id=int(query))
+    
     vector = SearchVector("name", "description")
-    query = SearchQuery("query")
-    return Products.objects.annotale(rank=SearchRank(vector, query)).order_by("-rank")
+    search_query = SearchQuery(query)  
+    
+    result = Products.objects.annotate(rank=SearchRank(vector, search_query)).filter(rank__gt=0).order_by("-rank")
+    
+   
+    result = result.annotate(headLine=SearchHeadline("name", search_query, start_sel='<span style="">'))
+    result = result.annotate(bodyLine=SearchHeadline("description", search_query, start_sel='<span style="">'))
+    
+    return result
 
 
     #keywords = [word for word in query.split() if len(word) > 2]
