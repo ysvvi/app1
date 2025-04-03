bd3881e314dc5ed36138db53d2b0c06ea997a6fb
ysvvi <nastasia.romanova.nastya2000@mail.ru>
2025-04-03 17:20:11 +0300

    login form and controller are ready

--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
+from django.contrib import auth
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from goods.models import Categories
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from users.forms import UserLoginForm
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password= request.POST['password'] 
+            user=auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+        
     context = {
         'title': ' Lilu – Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
