00715a770c90e2c064846923b60b7eaad145786f
ysvvi <nastasia.romanova.nastya2000@mail.ru>
2025-04-04 19:12:32 +0300

    cart remove controller added, commit before ajax

--- carts/views.py
+++ carts/views.py
@@ -79,13 +79,15 @@ def cart_change(request, product_slug):
 #     return JsonResponse(response_data)
 
 
-def cart_remove(request, product_slug):
-    ...
-    #     cart_id = request.POST.get("cart_id")
+def cart_remove(request, cart_id):
+    
+    #cart_id = request.POST.get("cart_id")
 
-    # cart = Cart.objects.get(id=cart_id)
+    cart = Cart.objects.get(id=cart_id)
     # quantity = cart.quantity
-    # cart.delete()
+    cart.delete()
+
+    return redirect(request.META['HTTP_REFERER'])
 
     # user_cart = get_user_carts(request)
 
