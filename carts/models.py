52c4a3206bd152f604ce947e7c305d6ee2d15211
ysvvi <nastasia.romanova.nastya2000@mail.ru>
2025-04-05 11:03:53 +0300

    admin panels setup

--- carts/models.py
+++ carts/models.py
@@ -39,4 +39,4 @@ class Cart(models.Model):
         if self.user:
             return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
             
-    # return f'Анонимная корзина | Товар {self.product.name} | Количество {self.quantity}'
+        return f'Анонимная корзина | Товар {self.product.name} | Количество {self.quantity}'
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from carts.admin import CartTabAdmin
+#from orders.admin import OrderTabulareAdmin   , OrderTabulareAdmin
 
 from users.models import User
 
-admin.site.register(User)
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email",]
+    search_fields = ["username", "email",]
+
+    inlines = [CartTabAdmin,]
